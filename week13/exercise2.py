b69756eb18087b7f280c32d2f00f94f4912d0300
Sebastian Saenz <sebastiansaenz2008@gmail.com>
2025-07-19 13:59:52 -0600

    fix: corrected exercises 2 and 3

--- week13/exercise2.py
+++ week13/exercise2.py
@@ -1,12 +1,17 @@
 
 
 def check_parameters_are_numbers(func):
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         try:
             integer_arguments = []
             for arg in args:
                 int_arg = int(arg)
                 integer_arguments.append(int_arg)
+
+            for kwarg in kwargs.values():
+                int_kwarg = int(kwarg)
+                if isinstance(int_kwarg, (int, float)):
+                    integer_arguments.append(kwarg)
             func(*integer_arguments)
         except ValueError as error:
             print(error)
@@ -21,4 +26,4 @@ def nums(*args):
         print(arg)
 
 
-nums(2, 3, 5, '4')
+nums(2, 3, 5, '4', number=7, string='9',)
--- week13/exercise3.py
+++ week13/exercise3.py
@@ -3,14 +3,12 @@ from datetime import date
 
 def check_is_adult(func):
     def wrapper(user, *args):
-        try:
-            if user.age >= 18:
-                print('this user is an adult')
-            else:
-                raise
-        except Exception:
+        if user.age >= 18:
+            print('this user is an adult')
+            return func(user, *args)
+        else:
             print('this user is not and adult')
-        
+
     return wrapper
 
 
@@ -23,13 +21,20 @@ class User:
         today = date.today()
         return ((today.year - self.date_of_birth.year - ((today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))))
 
-
-my_user = User(date(2015, 1, 1))
-print(f'age: {my_user.age}')
-
+print('adult')
+my_adult_user = User(date(1990, 1, 1))
+print(f'age: {my_adult_user.age}')
 
 @check_is_adult
 def only_adults_entry(user):
-    pass
+    print('starting the entry process')
 
-only_adults_entry(my_user)
+only_adults_entry(my_adult_user)
+
+print('------------------')
+
+print('not adult')
+my_not_adult_user = User(date(2020, 1, 1))
+print(f'age: {my_not_adult_user.age}')
+
+only_adults_entry(my_not_adult_user)
