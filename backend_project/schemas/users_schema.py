f07777eb14d3667f5a173d555b505d208ef339b7
Sebastian Saenz <sebastiansaenz2008@gmail.com>
2026-04-24 19:27:45 -0600

    add email and password validation to users_schema

--- backend_project/schemas/users_schema.py
+++ backend_project/schemas/users_schema.py
@@ -2,6 +2,7 @@
 
 from models import Users
 from extensions import ma
+from marshmallow import fields, validate
 
 
 
@@ -11,8 +12,9 @@ class UsersSchema(ma.SQLAlchemyAutoSchema):
         load_instance = True
 
     id = ma.auto_field(dump_only=True)
+    email = fields.Email(required=True, error_messages={'Invalid': 'Not a valid email address'})
+    password = fields.Str(required=True, load_only=True, validate=[validate.Length(min=8)])
     created_at = ma.auto_field(load_only=True)
-    password = ma.auto_field(load_only=True)
 
     roles = ma.Nested('RolesSchema', many=True)
     
